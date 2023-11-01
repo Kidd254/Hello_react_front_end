dddc05e8b04b7184407c6ffc425e3e9bcc432780
Lawrence Muema Kioko <lorenkioko@gmail.com>
2023-11-01 13:02:15 +0300

    Create a page for displaying retrieved data

---
+++ src/components/Greetings.jsx
@@ -0,0 +1,43 @@
+import { useDispatch, useSelector } from 'react-redux';
+import React, { useEffect } from 'react';
+import { fetchGreetings } from '../redux/greetings/greetingsSlice';
+
+function Greeting() {
+  console.log('Greeting component is rendering.');
+  const { greetings, isLoading, error } = useSelector(
+    (state) => state.greetings
+  );
+  const dispatchActions = useDispatch();
+
+  useEffect(() => {
+    dispatchActions(fetchGreetings());
+  }, [dispatchActions]);
+
+  let loadMessage = null;
+
+  if (isLoading) {
+    loadMessage = 'Loading message...';
+  }
+
+  if (error) {
+    loadMessage = 'Error loading data';
+  }
+
+  return (
+    <section>
+      {isLoading || error ? (
+        <p className="status">{loadMessage}</p>
+      ) : (
+        <div>
+          {greetings.map((greeting) => (
+            <p key={greeting.id} className="greeting-message">
+              {greeting.content}
+            </p>
+          ))}
+        </div>
+      )}
+    </section>
+  );
+}
+
+export default Greeting;
